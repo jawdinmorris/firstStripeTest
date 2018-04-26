d952fcead65da2ce2ad17b585c121a62ec1b2043
jawdinmorris <jordanandrewmorris1@gmail.com>
2018-04-26 13:04:06 +1000

    Finished adding stripe payment as simple link

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,29 @@
+class ChargesController < ApplicationController
+  before_action :set_amount
+  def new
+  end
+
+  def create
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'aud'
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+
+  private
+  def set_amount
+    @amount = 250
+  end
+
+end
